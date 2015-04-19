cb68d43cd25063f5dbae2ad3b5c4a06ee01999c6
1310650419 <85238305@qq.com>
2015-04-19 19:29:33 +0800

    Delete main.py

--- 01/main.py
+++
@@ -1,8 +0,0 @@
-# isdhw
-for i in range(1,10):
-	print("  "*8*(i-1),end='')
-	for j in range(i,10):
-		if((i*j)<10):print(i,'*',j,'=',i*j,"      ",end='')
-		else:print(i,'*',j,'=',i*j,"     ",end='')
-	print(end='\n')
-input()
